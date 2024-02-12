692f7f750712e508513ed6475615f2fbda74d3da
Mariam Shahir <mariamshahir2000@gmail.com>
2024-02-12 19:27:04 +0200

    Finish splash screen. Add home screen file.

---
+++ lib/splash.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:islamic/homescreen.dart';
+import 'package:islamic/utils/app_assets.dart';
+
+class Splash extends StatefulWidget {
+  static const String routeName = "splash";
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: const BoxDecoration(
+          image: DecorationImage(image: AssetImage(AppAssets.splash)),
+        ),
+      ),
+    );
+  }
+@override
+  void initState() {
+    super.initState();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersive);
+    Future.delayed(Duration(seconds: 2),(){
+      Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (_)=>HomeScreen(), ));
+    });
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,overlays: SystemUiOverlay.values);
+  }
+}
