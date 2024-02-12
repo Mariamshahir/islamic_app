fa2bde8719d944863164456c77f19362e9f7d79f
Mariam Shahir <mariamshahir2000@gmail.com>
2024-02-12 22:12:25 +0200

    Finished bottom navigation in home screen. Add 4 taps (quran-ahadeth-radio-sebha).

--- lib/homescreen.dart
+++ lib/homescreen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:islamic/taps/quran/qurantab.dart';
-import 'package:islamic/taps/radio/radiotap.dart';
 import 'package:islamic/taps/ahadeth/ahadethtab.dart';
+import 'package:islamic/taps/radio/radiotap.dart';
 import 'package:islamic/taps/sebha/sebhatab.dart';
 import 'package:islamic/utils/aap_theme.dart';
 import 'package:islamic/utils/app_assets.dart';
@@ -16,57 +16,62 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   int currentTapIndex = 0;
-  Widget bady =Container();
+  List<Widget> bodyContent = [ RadioTab(), SebhaTab(), AhadethTab(), QuranTab(),];
+
   @override
   Widget build(BuildContext context) {
     return Container(
       decoration: const BoxDecoration(
         image: DecorationImage(image: AssetImage(AppAssets.background)),
       ),
-        child: Scaffold(
-          backgroundColor: AppColors.transparent,
-          appBar: buildAppBar(),
-          bottomNavigationBar: buildBottomNavigation(),
-          body: bady,
-        ));
+      child: Scaffold(
+        backgroundColor: AppColors.transparent,
+        appBar: buildAppBar(),
+        bottomNavigationBar: buildBottomNavigation(),
+        body: bodyContent[currentTapIndex],
+      ),
+    );
   }
 
   AppBar buildAppBar() => AppBar(
-          backgroundColor: AppColors.transparent,
-          centerTitle: true,
-          elevation: 0,
-          title: const Text("إسلامي", style: AppTheme.appBarTextStyle,),
+    backgroundColor: AppColors.transparent,
+    centerTitle: true,
+    elevation: 0,
+    title: const Text(
+      "إسلامي",
+      style: AppTheme.appBarTextStyle,
+    ),
   );
 
   Widget buildBottomNavigation() => Theme(
-      data: ThemeData(
-        canvasColor: AppColors.orange,
-      ),
-      child: BottomNavigationBar(
-        items: [
-          buildBottomNavigationBarItem(AppAssets.iconRadio,"الراديو"),
-          buildBottomNavigationBarItem(AppAssets.iconSebha,"التسبيح"),
-          buildBottomNavigationBarItem(AppAssets.iconAhadeth,"الاحاديث"),
-          buildBottomNavigationBarItem(AppAssets.iconQuran,"القران")
-        ],
-        selectedItemColor: AppColors.lightBlack,
-        unselectedItemColor: AppColors.white,
-        currentIndex: currentTapIndex,
-        onTap: (index){
-          currentTapIndex=index;
-          if(currentTapIndex == 0){
-            bady = RadioTab();
-          }else if (currentTapIndex == 1){
-            bady = SebhaTab();
-          }else if(currentTapIndex == 2){
-            bady = AhadethTab();
-          }else{
-            bady = QuranTab();
-          }
-          setState(() {});
-        },
-  ));
+    data: ThemeData(
+      canvasColor: AppColors.orange,
+    ),
+    child: BottomNavigationBar(
+      items: [
+        buildBottomNavigationBarItem(AppAssets.iconRadio, "الراديو"),
+        buildBottomNavigationBarItem(AppAssets.iconSebha, "التسبيح"),
+        buildBottomNavigationBarItem(AppAssets.iconAhadeth, "الاحاديث"),
+        buildBottomNavigationBarItem(AppAssets.iconQuran, "القران"),
+      ],
+      selectedItemColor: AppColors.lightBlack,
+      unselectedItemColor: AppColors.white,
+      currentIndex: currentTapIndex,
+      onTap: (index) {
+        setState(() {
+          currentTapIndex = index;
+        });
+      },
+    ),
+  );
 
-  BottomNavigationBarItem buildBottomNavigationBarItem(String imagePath,String label) =>
-      BottomNavigationBarItem(icon: ImageIcon(AssetImage(imagePath),size: 32,),label: label);
+  BottomNavigationBarItem buildBottomNavigationBarItem(
+      String imagePath, String label) =>
+      BottomNavigationBarItem(
+        icon: ImageIcon(
+          AssetImage(imagePath),
+          size: 32,
+        ),
+        label: label,
+      );
 }
