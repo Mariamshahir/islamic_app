f5b17d89776b27418df26dcd91019a04ecd29688
Mariam Shahir <mariamshahir2000@gmail.com>
2024-02-22 18:52:14 +0200

    Finished Language Provider.

---
+++ lib/provider/language_provider.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class LanguageProvider extends ChangeNotifier{
+  String currentLocale = "en";
+
+  void setCurrentLocale(String newLocale){
+    currentLocale = newLocale;
+    notifyListeners();
+  }
+
+}
